3d52327bbab15024241e4896906e50c0379e5d88
Dhiraj Shirse <dhirajshirse2006@gmail.com>
2026-01-02 15:22:16 +0000

    Basic setup done

---
+++ inheritance-app/src/pages/Home/index.jsx
@@ -0,0 +1,3 @@
+export default function Landing() {
+  return <h1>It works 🎉</h1>;
+}
---
+++ inheritance-app/src/routes.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import PublicLayout from "layouts/PublicLayout";
+import Home from "pages/Home";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route  element={<PublicLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
